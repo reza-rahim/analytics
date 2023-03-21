56f693396b09b15efbc7c6bf5622fd2131ab7ca3
root <root@minio-bastion.c.redislabs-sa-training-services.internal>
2023-03-21 04:59:35 +0000

    history

--- ansible/02.s3bucket.sh
+++ ansible/02.s3bucket.sh
@@ -1,3 +1,4 @@
 mc alias set s3  http://minio-node-1:9000 minioadmin minioadmin
 mc mb s3/spark
 mc mb s3/warehouse
+mc cp 02.s3bucket.sh s3/spark/logs/init/02.s3bucket.sh
---
+++ ansible/06.user.sh
@@ -0,0 +1,3 @@
+ssh minio-node-1
+useradd -g users -G application -m  -d /home/demo  demo
+passwd demo 
