ac050192df15f3a5225a5ce4c5f9b6c96016d187
root <root@analytic-bastion.c.redislabs-sa-training-services.internal>
2023-03-29 15:24:09 +0000

    minio

--- ansible/04.s3bucket.sh
+++ ansible/04.s3bucket.sh
@@ -1,4 +1,4 @@
-mc alias set s3  http://node-1:9000 minioadmin minioadmin
+mc --insecure alias set s3   https://minio.mnbvcxz.com:9000  minioadmin minioadmin
 mc mb s3/spark
 mc mb s3/warehouse
 mc cp 02.s3bucket.sh s3/spark/logs/init/02.s3bucket.sh
