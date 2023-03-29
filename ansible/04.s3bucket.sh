8c1da07775b86421f827e4f590b054204fe0f3bf
root <root@analytic-bastion.c.redislabs-sa-training-services.internal>
2023-03-29 17:13:54 +0000

    cert fix

--- ansible/04.s3bucket.sh
+++ ansible/04.s3bucket.sh
@@ -1,4 +1,4 @@
-mc --insecure alias set s3   https://minio.mnbvcxz.com:9000  minioadmin minioadmin
+mc  alias set s3   https://minio.mnbvcxz.com:9000  minioadmin minioadmin
 mc mb s3/spark
 mc mb s3/warehouse
 mc cp *.s3bucket.sh s3/spark/logs/init/s3bucket.sh
