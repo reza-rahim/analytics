935172819a87b2f2b895934b5ce0bc89b21b0527
root <root@analytic-bastion.c.redislabs-sa-training-services.internal>
2023-04-13 18:20:59 +0000

    key

--- ansible/03.patroni.sh
+++ ansible/03.patroni.sh
@@ -1,7 +1,8 @@
 source env.sh ;
 ansible-playbook -i inventory.ini \
 -e db_user=${DB_USER}  \
--e db_password=${DB_PASSWORD}  \
--e replication_user=${REPLICATION_USER}  \
--e replication_password=${REPLICATION_PASSWORD}  \
+-e patroni_superuser_username=${DB_USER}  \
+-e patroni_superuser_password=${DB_PASSWORD}  \
+-e patroni_replication_user=${PATRONI_REPLICATION_USER}  \
+-e patroni_replication_password=${PATRONI_REPLICATION_PASSWORD}  \
 patroni.yaml
